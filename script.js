94fc29b393571c29b1766abebb86f6818c6f4de0
Arjun Maudhoo <apm526@nyu.edu>
2022-10-27 18:46:27 -0400

    Rearrange functions, grid updating added
    The functions are reorganized to work with the grid updating better

--- script.js
+++ script.js
@@ -1,37 +1,49 @@
 const container = document.querySelector('.container');
 
-function createGrid () {
-    for (let i = 0; i < 256; i++){
+function createGrid (gridNumber) {
+    for (let i = 0; i < (gridNumber*gridNumber); i++){
         const div = document.createElement("div");
         div.classList.add("square");
         container.appendChild(div);
     }
 }
-createGrid();
+createGrid(16);
 
-// function updateGrid (gridNumber) {
-    
-// }
+function coloring () {
+    var squares = document.querySelectorAll('.square')
+    for(let i = 0; i < squares.length; i++) {
+        let square = squares[i];
+        square.addEventListener("mouseover", function(e) {
+            e.target.classList.replace('square', 'coloredSquare');
+        });
+    }
+}
+coloring();
 
+function updateGrid (gridNumber) {
+    container.innerHTML = "";
+    container.style.setProperty("grid-template-columns", `repeat(${gridNumber}, 2fr)`);
+    container.style.setProperty("grid-template-rows", `repeat(${gridNumber}, 2fr)`);
+    createGrid(gridNumber);
+    coloring();
+}
 
 const btn = document.querySelector(".sizeButton");
 btn.addEventListener('click', function(){
     var gridNumber = parseInt(prompt("Enter a number!"));
-    if (Number.isInteger(gridNumber)) {
-        console.log('updated grid');
-        updateGrid(gridNumber);
+    if (gridNumber > 1000) {
         return;
     }
-        console.log("not a number");
-        return;
+    else {
+        if (Number.isInteger(gridNumber)) {
+            console.log('updated grid');
+            updateGrid(gridNumber);
+            return;
+        }
+            console.log("not a number");
+            return;
+    }
 });
 
-const squares = document.querySelectorAll('.square')
-for(let i = 0; i < squares.length; i++) {
-    let square = squares[i];
-    square.addEventListener("mouseover", function(e) {
-        e.target.classList.replace('square', 'coloredSquare');
-    });
-}
 
 
